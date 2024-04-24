16057045dd69c674ebb0422f3c9739b42b107719
Thomas <tommyvalotta@gmail.com>
2024-04-23 21:16:04 -0400

    Added one counter component to the main app which updates the DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ColourPicker from './ColourPicker.jsx'
+import Counter from './Counter.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ColourPicker/>
+    <ColourPicker />
+    <Counter />
   </React.StrictMode>,
 )
