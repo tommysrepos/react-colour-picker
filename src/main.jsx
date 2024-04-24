4b9eda94bfee2d89367dfef861d796d6df4f14a3
Thomas <tommyvalotta@gmail.com>
2024-04-23 22:08:41 -0400

    Added a FavouritePokemon component to the DOM

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import ColourPicker from './ColourPicker.jsx'
 import Counter from './Counter.jsx'
 import './index.css'
+import FavouritePokemon from './FavouritePokemon.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ColourPicker />
     <Counter />
+    <FavouritePokemon />
   </React.StrictMode>,
 )
