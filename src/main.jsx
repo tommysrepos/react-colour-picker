fb31c943db61958eeea79f307e74f074c69c5077
Thomas <tommyvalotta@gmail.com>
2024-04-25 10:21:07 -0400

    Added a FruitList component to the react DOM

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import ColourPicker from './ColourPicker.jsx'
 import Counter from './Counter.jsx'
 import './index.css'
 import FavouritePokemon from './FavouritePokemon.jsx'
+import FruitList from './FruitList.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ColourPicker />
     <Counter />
     <FavouritePokemon />
+    <FruitList />
   </React.StrictMode>,
 )
